6d1eacdb3e9727dd973f464acf50a7ad03f316cd
Darius Kruythoff <dkruythoff@gmail.com>
2015-03-11 10:16:30 +0100

    Passing tests through gulp to facilitate building on Windows

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp'),
 	inlineimage = require('./index.js'),
-	path = require('path');
+	path = require('path'),
+	$ = require('gulp-load-plugins')();
 
 gulp.task('default', function () {
 	return gulp.src(path.join('test','css','style.css'))
 		.pipe(inlineimage())
 		.pipe(gulp.dest(path.join('build', 'css')));
+});
+
+gulp.task('test', function() {
+	return gulp.src('test/test.js').pipe($.mocha({
+		reporter: 'spec'
+	}));
 });
